b7b0164360876a05fe95adb13773dd98c079073b
Talerchik-Aleksey <atalerchik@twelvedevs.com>
2023-05-10 18:57:26 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["react-dom/client"],
+      external: ["react", "react-dom", "react-dom/client"],
     },
   },
 });
